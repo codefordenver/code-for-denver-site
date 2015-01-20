7de79536d730ec4548710ecf87b28ea8bc0fa2ac
david viramontes <dviramontes@gmail.com>
2015-01-20 01:20:14 -0700

    + spell check

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ import snabbt from 'snabbt.js';
 
 var ele = $('#is-this-you');
 
-const roles = ['a developer','a community organizer', 'a graphic-designer', 'a facillitator', 'a project-manager', 'a goverment representative', 'a student', 'an ally', 'curious about Code For Denver', 'a technical writer'] ;
+const roles = ['a developer','a community organizer', 'a graphic-designer', 'a facilitator', 'a project-manager', 'a government representative', 'a student', 'an ally', 'curious about Code For Denver', 'a technical writer'] ;
 
 var randomRole = () => {
 	return roles[0 | Math.random() * roles.length];
