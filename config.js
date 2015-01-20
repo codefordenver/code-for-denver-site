2c00fdc828ec16cc0f0b2f485ea8ec7a4ad0d6a2
david viramontes <dviramontes@gmail.com>
2015-01-20 00:53:07 -0700

    adds jspm and system.js deps

---
+++ config.js
@@ -0,0 +1,14 @@
+System.config({
+  "paths": {
+    "*": "*.js",
+    "code-for-denver-site/*": "js/*.js",
+    "github:*": "jspm_packages/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "snabbt.js": "github:daniel-lundin/snabbt.js@0.3.0"
+  }
+});
+
