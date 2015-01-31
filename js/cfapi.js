dc3f3b38007a2053cae39a8e6e6889204f1d3a12
BRANDON BARRETT <brandonbarrett@BRANDONs-MacBook-Pro.local>
2015-01-31 08:50:22 -0700

    added handlebars-v2.0.0 and implemented compile-on-fly template for the meetup tiles

--- js/cfapi.js
+++ js/cfapi.js
@@ -3,26 +3,25 @@
 * See https://github.com/codeforamerica/cfapi for an overview
 * See http://codeforamerica.org/api/ for full documentation
 */
-
 (function($, window) {
   $.getJSON( "http://codeforamerica.org/api/organizations/Code-for-Denver", function( data ) {
     var events = [];
 
-    $(data.current_events).each( function() {
-      var mEvent = this, name, time, image, link, linkText, tile;
-      name = "<div class='event-name'>" + mEvent.name + "</div>";
-      time = "<time datetime="+mEvent.start_time+">" + (function(){ return new Date(mEvent.start_time).toLocaleString();})() + "</time>";
-      image = "<img src='img/meetup-icon.png'/>";
-      linkText = "<p>view on meetup.com</p>";
-      link = "<a href=" + mEvent.event_url + " >" + image + linkText + "</a>";
-      tile = name + time + link;
-      events.push("<li class='event'>" + tile + "</li>");
+    //  usage: {{dateFormat myDateString}}
+    Handlebars.registerHelper('dateFormat', function(context, block) {
+      return new Date(context).toLocaleString();
     });
 
-    $( "<ul/>", {
-      "class": "my-new-list",
-      html: events.join("")
-    }).appendTo("div#meetups-container");
+    var meetupTemplate = "<ul>{{#current_events}}<li class='event'>"+
+      "<div class='event-name'>{{name}}</div>"+
+      "<time datetime={{start_time}}>{{dateFormat start_time}}</time>" +
+      "<a href='{{event_url}}'><img src='img/meetup-icon.png'/><p>view on meetup.com</p></a>" +
+      "</li>{{/current_events}}</ul>";
+    var template = Handlebars.compile(meetupTemplate);
+
+    var result = template(data);
+
+    $(result).appendTo("div#meetups-container");
 
   });
 })($, window);
