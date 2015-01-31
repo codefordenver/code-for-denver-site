04e0697b091b541a1c5b901f4115ddf0bafb7580
Willy Douglas <willygdouglas@gmail.com>
2015-01-31 14:37:06 -0700

    Add project slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+// Project carousel stuff
+$(document).on('slide.bs.carousel', '#carousel-example-generic', function(e) {
+  var slideIndex = $(e.relatedTarget).index();
+
+  var $projectList = $('.projects')
+  $projectList.find('.active').removeClass('active');
+  $projectList.children().eq(slideIndex).addClass('active');
+});
+
 
