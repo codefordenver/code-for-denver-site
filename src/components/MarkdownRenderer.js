82437f15ef29a5beceeeceef425fea0f9946c7de
Willy Douglas <willygdouglas@gmail.com>
2015-12-13 17:29:35 -0700

    Add 'suggest edit' option for markdown content

--- src/components/MarkdownRenderer.js
+++ src/components/MarkdownRenderer.js
@@ -3,7 +3,12 @@ import React from 'react';
 const MarkdownRenderer = ({fileName}) => {
   const markdownFile = require(`../markdown/${fileName}.md`);
   return (
-    <div dangerouslySetInnerHTML={{__html: markdownFile}} />
+    <div className="markown-renderer">
+      <a className="markdown-edit" href={`https://github.com/codefordenver/code-for-denver-site/edit/master/src/markdown/${fileName}.md`}>
+        suggest edit
+      </a>
+      <div dangerouslySetInnerHTML={{__html: markdownFile}} />
+    </div>
   );
 };
 
