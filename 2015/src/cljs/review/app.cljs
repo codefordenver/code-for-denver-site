41856d4b495d17631516b6983112bec7dd4788be
dviramontes <dviramontes@gmail.com>
2016-01-06 23:01:56 -0700

    adds boot setup, adds 2015 review basic layout

---
+++ 2015/src/cljs/review/app.cljs
@@ -0,0 +1,71 @@
+(ns review.app
+  (:require [reagent.core :as reagent :refer [atom]]))
+
+(defn some-component []
+  [:div.main
+   [:div.container-fluid
+    [:div.row
+     [:div.col-lg-12
+      [:p.lead "Code For Denver is proud to present:"]
+      [:h1.super "2015"]
+      [:h3 "END OF YEAR SUMMARY"]]]
+    [:div.row
+     [:div.col-lg-12
+      [:p.lead.text-center "where we started.."]
+      [:h3 "STAT 1"]
+      [:p.lead
+       "Lorem ipsum dolor sit amet,
+       consectetur adipiscing elit.
+        Duis pharetra varius quam sit
+         amet vulputate. Quisque mauris
+          augue, molestie tincidunt
+          condimentum vitae, gravida a
+           libero. Aenean sit amet felis
+           dolor, in sagittis nisi. Sed
+           ac orci quis tortor imperdiet
+            venenatis. Duis elementum
+            auctor accumsan. Aliquam in
+            felis sit amet augue."]]]
+    [:div.row
+     [:div.col-lg-12
+      [:h3.text-center "STAT 2"]
+      [:p.lead
+       "Lorem ipsum dolor sit amet,
+       consectetur adipiscing elit.
+        Duis pharetra varius quam sit
+         amet vulputate. Quisque mauris
+          augue, molestie tincidunt
+          condimentum vitae, gravida a
+           libero. Aenean sit amet felis
+           dolor, in sagittis nisi. Sed
+           ac orci quis tortor imperdiet
+            venenatis. Duis elementum
+            auctor accumsan. Aliquam in
+            felis sit amet augue."]]]
+    [:div.row
+     [:div.col-lg-12
+      [:h3.text-right "STAT 3"]
+      [:p.lead
+       "Lorem ipsum dolor sit amet,
+       consectetur adipiscing elit.
+        Duis pharetra varius quam sit
+         amet vulputate. Quisque mauris
+          augue, molestie tincidunt
+          condimentum vitae, gravida a
+           libero. Aenean sit amet felis
+           dolor, in sagittis nisi. Sed
+           ac orci quis tortor imperdiet
+            venenatis. Duis elementum
+            auctor accumsan. Aliquam in
+            felis sit amet augue."]]]
+
+    [:div.row
+     [:div.col-lg-12
+      [:h3.text-left "MANY THANKS!"]]]]])
+
+(defn calling-component []
+  [:div [some-component]])
+
+(defn init []
+  (reagent/render-component [calling-component]
+                            (.getElementById js/document "container")))
