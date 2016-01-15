ef03b10e18bf25bb63f65c550bad88f9c0bfb531
dviramontes <dviramontes@gmail.com>
2016-01-14 22:07:51 -0700

    adds initial sparktime and github data

--- 2015/src/cljs/review/app.cljs
+++ 2015/src/cljs/review/app.cljs
@@ -7,11 +7,24 @@
 ;; -------------------------------
 ;; define app state
 
-(def colors {:primary   "ghostwhite"
-             :secondary "rgba(100,100,100,1.0)"})
+(def colors {:primary   "white"
+             :secondary "rgba(100,100,100, 0.75)"})
 
 (def app-state (r/atom {:titles (:primary colors)
                         :body   (:secondary colors)}))
+
+(def titles ["Code Across 2015", "Sol Cavp", "Fresh Food Connect API", "Fresh Food Connect", "Code for Denver Site", "Denver Sustainability", "RMFU Feed"]) ;
+
+(def contributors [3, 5, 8, 9, 9, 10, 14])
+
+(def data {:labels   titles
+           :datasets [{
+                       :label           "Code for Denver Contributors"
+                       :fillColor       "white"
+                       :strokeColor     "white"
+                       :highlightFill   "white"
+                       :highlightStroke "rgba(220,220,220,1)"
+                       :data            contributors}]})
 
 ;; -------------------------------
 ;; register media queries triggers
@@ -26,71 +39,54 @@
                                   (swap! app-state assoc-in [:titles] (:primary colors))
                                   (print "unmatched"))}))
 
-(add-watch app-state :logger #(-> %4 clj->js js/console.log))
+;(add-watch app-state :logger #(-> %4 clj->js js/console.log))
 
 (defn some-component []
-      (let []
-           [:div.main
-            [:div.row.intro
-             [:div.col-lg-12
-              [:p.lead "Code For Denver is proud to present:"]
-              [:h1.super {:style {:color (:titles @app-state)}} "2015"]
-              [:h2 {:style {:color (:titles @app-state)}} "END OF YEAR SUMMARY"]]]
-            [:div.row.part-two
-             [:div.col-lg-12
-              [:p.lead.text-center "where we started.."]
-              [:h3 "STAT 1"]
-              [:p.lead
-               "Lorem ipsum dolor sit amet,
-               consectetur adipiscing elit.
-                Duis pharetra varius quam sit
-                 amet vulputate. Quisque mauris
-                  augue, molestie tincidunt
-                  condimentum vitae, gravida a
-                   libero. Aenean sit amet felis
-                   dolor, in sagittis nisi. Sed
-                   ac orci quis tortor imperdiet
-                    venenatis. Duis elementum
-                    auctor accumsan. Aliquam in
-                    felis sit amet augue."]]]
-            [:div.row
-             [:div.col-lg-12
-              [:h3.text-center "STAT 2"]
-              [:p.lead
-               "Lorem ipsum dolor sit amet,
-               consectetur adipiscing elit.
-                Duis pharetra varius quam sit
-                 amet vulputate. Quisque mauris
-                  augue, molestie tincidunt
-                  condimentum vitae, gravida a
-                   libero. Aenean sit amet felis
-                   dolor, in sagittis nisi. Sed
-                   ac orci quis tortor imperdiet
-                    venenatis. Duis elementum
-                    auctor accumsan. Aliquam in
-                    felis sit amet augue."]]]
-            [:div.row
-             [:div.col-lg-12
-              [:h3.text-right "STAT 3"]
-              [:p.lead
-               "Lorem ipsum dolor sit amet,
-               consectetur adipiscing elit.
-                Duis pharetra varius quam sit
-                 amet vulputate. Quisque mauris
-                  augue, molestie tincidunt
-                  condimentum vitae, gravida a
-                   libero. Aenean sit amet felis
-                   dolor, in sagittis nisi. Sed
-                   ac orci quis tortor imperdiet
-                    venenatis. Duis elementum
-                    auctor accumsan. Aliquam in
-                    felis sit amet augue."]]]
+      (r/create-class
+        {:component-did-mount (fn []
+                                  (let [ctx (.getContext (.getElementById js/document "myChart") "2d")]
+                                       (print ctx)
+                                       (.Bar (js/Chart. ctx) (clj->js data))))
+         :reagent-render
+                              (fn []
+                                  [:div.main
+                                   [:div.row.intro
+                                    [:div.col-lg-12
+                                     [:p.lead "Code For Denver is proud to present:"]
+                                     [:h1.super {:style {:color (:titles @app-state)}} "2015"]
+                                     [:h2 "END OF YEAR SUMMARY"]]]
 
-            [:div.row.thanks
-             [:div.col-lg-12
-              [:h3.text-center.page-header "MANY THANKS!"]
-              [:div.list-group
-               [:button.list-group-item.btn-default "test"]]]]]))
+                                   [:div.row.part-two
+                                    [:div.col-lg-12
+                                     [:p.lead.text-center "where we started.."]
+                                     [:h3 "contributors"]
+
+                                     [:canvas {:id     "myChart"
+                                               :width  "400"
+                                               :height "400"}]
+                                     [:p.lead
+                                      "embarked on 7 major projects.."]]]
+
+                                   [:div.row.part-three
+                                    [:div.col-lg-12
+                                     [:h3.text-center "3000 hours"]
+                                     [:h3.text-center "75 work weeks"]
+                                     [:h3.text-center "1.5 years"]
+                                     [:p.lead.text-center
+                                      [:a {:href "http://sparktime.org/"
+                                           :style {:color "white"}} "sparktime.org"]]]]
+
+                                   [:div.row.part-four
+                                    [:div.col-lg-12
+                                     [:h3.text-right "47"]
+                                     [:p.lead.text-right
+                                      "meetup events"]]]
+
+                                   [:div.row.thanks
+                                    [:div.col-lg-12
+                                     [:h3.text-center.page-header "MANY THANKS!"]
+                                     [:div.list-group
+                                      [:button.list-group-item.btn-default "test"]]]]])}))
 
 (defn main-component []
       [:div [some-component]])
