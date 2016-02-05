226375ffaa9c46fb6b4ed3d08470efb39defda88
Dsenza <ddescenza@gmail.com>
2016-02-05 14:48:12 -0700

    Worked on issue 134, adding the GetInvolved component into the Volunteer component.

--- src/components/home/Volunteer.js
+++ src/components/home/Volunteer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import GetInvolved from './GetInvolved'
 import MarkdownRenderer from '../MarkdownRenderer';
 
 const Volunteer = () => (
 
 <section id="about" className="standard primary">
+  <GetInvolved />
   <MarkdownRenderer fileName="volunteer" />
 </section>
 
