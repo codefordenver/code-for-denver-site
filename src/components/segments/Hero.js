0f306a05a862cc6680c4581eb2d136124b2a5cdd
Zachary Rose <zachary.f.rose@gmail.com>
2016-02-09 23:38:12 -0700

    Changed Hero Component to Take Dynamic values

--- src/components/segments/Hero.js
+++ src/components/segments/Hero.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
-const Hero = () => (
-  <section className="hero">
-    <div className="intro">
-      <h1>We Are Code For Denver.</h1>
-      <p><a href="#">We meet almost every Monday. Join us, everyone is welcome!</a></p>
-    </div>
-  </section>
-);
+class Hero extends React.Component {
+  render (){
+
+    return (
+      <section className="hero" style = {{background: 'url("../static/img/' + this.props.background + '") no-repeat center center', backgroundSize: 'cover'}} >
+        {console.log(this.props)}
+        <div className="intro">
+          <h1>{this.props.title}</h1>
+          {this.props.content}
+        </div>
+      </section>
+    );
+
+  }
+}
 
 export default Hero;
