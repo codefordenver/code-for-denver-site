41fec86bd3d2e46a35c8c07f31602cf78d0b3a48
Ryan Bird <ryan01bird@gmail.com>
2016-02-22 21:01:44 -0700

    added link text

---
+++ src/components/segments/CodeAcrossLink.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CodeAcrossLink = () => (
+
+  <section className="getconnected-links">
+    <h1>Code Across 2016</h1>
+    <p>Join us on Saturday March 5, 2016</p>
+    <a href='http://www.meetup.com/CodeForDenver/' >Code Across 2016</a>
+  </section>
+
+);
+
+export default CodeAcrossLink;
--- src/components/segments/Interested.js
+++ src/components/segments/Interested.js
@@ -12,29 +12,38 @@ else
   nextMeeting = 'next monday';
 }
 
-const Interested = () => (
-  <section className="standard interested">
-  {console.log(nextMeeting)}
-    <div className="flexgrid-row">
-      <div className="interested-title">
-        <h1>Are You Interested?</h1>
-        <h2>Good! We can use your help.</h2>
-      </div>
-      <div className="interested-calendar">
+  class Interested extends React.Component {
+
+    render() {
+
+      return (
+        <section className="standard interested">
+        {console.log(nextMeeting)}
           <div className="flexgrid-row">
-            <div className="calendar-date">
-              <div className="month">{Date.create(nextMeeting).format('{Month}')}</div>
-              <div className="day">{Date.create(nextMeeting).format('{d}')}</div>
+            <div className="interested-title">
+              <h1>Are You Interested?</h1>
+              <h2>Good! We can use your help.</h2>
             </div>
-            <div className="calendar-text">
-              <a href="http://www.meetup.com/CodeForDenver/" target="_blank" className="meetup-btn">Join Us for our next Project Night</a>
-              <p className="date">{Date.create(nextMeeting).format('{Weekday}, {Month} {d}, {yyyy}')}
-              <br />6:00 PM to 9:00PM</p>
+            <div>
+              { this.props.children }
+            </div>
+            <div className="interested-calendar">
+                <div className="flexgrid-row">
+                  <div className="calendar-date">
+                    <div className="month">{Date.create(nextMeeting).format('{Month}')}</div>
+                    <div className="day">{Date.create(nextMeeting).format('{d}')}</div>
+                  </div>
+                  <div className="calendar-text">
+                    <a href="http://www.meetup.com/CodeForDenver/" target="_blank" className="meetup-btn">Join Us for our next Project Night</a>
+                    <p className="date">{Date.create(nextMeeting).format('{Weekday}, {Month} {d}, {yyyy}')}
+                    <br />6:00 PM to 9:00PM</p>
+                  </div>
+                </div>
             </div>
           </div>
-      </div>
-    </div>
-  </section>
-);
+        </section>
+      );
+    }
+  }
 
 export default Interested;
