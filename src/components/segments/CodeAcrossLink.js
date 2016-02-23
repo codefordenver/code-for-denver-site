99ab36433af4ff710b3f8909807f47654a2c5821
Dsenza <ddescenza@gmail.com>
2016-02-23 12:50:45 -0700

    Tweak codeacross styles and component.

--- src/components/segments/CodeAcrossLink.js
+++ src/components/segments/CodeAcrossLink.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const CodeAcrossLink = () => (
 
-  <section className="getconnected-links">
+  <div className="interested-codeacross">
     <h1>Code Across 2016</h1>
-    <p>Join us on Saturday March 5, 2016</p>
-    <a href='http://www.meetup.com/CodeForDenver/events/228539039/' >Code Across 2016</a>
-  </section>
+    <h2>Join us on Saturday March 5, 2016</h2>
+    <a href='http://www.meetup.com/CodeForDenver/events/228539039/' target='blank' >Check out the event here!</a>
+  </div>
 
 );
 
