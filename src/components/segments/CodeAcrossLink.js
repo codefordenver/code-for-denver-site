cf34d8ce72fd58f7edd91a3bbcb71c2a0488d425
Ryan Bird <ryan01bird@gmail.com>
2016-02-23 19:13:44 -0700

    edited link text

--- src/components/segments/CodeAcrossLink.js
+++ src/components/segments/CodeAcrossLink.js
@@ -4,8 +4,8 @@ const CodeAcrossLink = () => (
 
   <section className="getconnected-links">
     <h1>Code Across 2016</h1>
-    <p>Join us on Saturday March 5, 2016</p>
-    <a href='http://www.meetup.com/CodeForDenver/events/228539039/' >Code Across 2016</a>
+    
+    <a href='http://www.meetup.com/CodeForDenver/events/228539039/' ><h2>Join us on Saturday March 5, 2016</h2></a>
   </section>
 
 );
