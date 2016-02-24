3de083914d6dbaf258ba34509f1a8ae8df8cef4e
Dsenza <ddescenza@gmail.com>
2016-02-23 19:37:13 -0700

    Fix HTML tag in CodeAcrossLink.js

--- src/components/segments/CodeAcrossLink.js
+++ src/components/segments/CodeAcrossLink.js
@@ -4,9 +4,8 @@ const CodeAcrossLink = () => (
 
   <div className="interested-codeacross">
     <h1>Code Across 2016</h1>
-  
     <a href='http://www.meetup.com/CodeForDenver/events/228539039/' ><h2>Join us on Saturday March 5, 2016</h2></a>
-  </section>
+  </div>
 
 );
 
