cde2923b507bb162dba92d68087813da1d19d618
Zachary Rose <zachary.f.rose@gmail.com>
2016-02-29 17:08:19 -0700

    fixed social links, added dynamic mapping

--- src/components/segments/SocialMediaLinks.js
+++ src/components/segments/SocialMediaLinks.js
@@ -1,15 +1,27 @@
 import React from 'react';
 
+const socialLinks = [
+  { link: "https://www.facebook.com/hashtag/codefordenver", icon: "fa fa-facebook-official"},
+  { link: "https://twitter.com/codefordenver", icon: "fa fa-twitter-square"},
+  { link: "https://github.com/codefordenver/", icon: "fa fa-github-square"}/*,
+  { link: "#", icon: "fa fa-pinterest-square"},
+  { link: "#", icon: "fa fa-google-plus-square"},
+  { link: "#", icon: "fa fa-youtube-square"}
+  */
+]
+
+
 const SocialMediaLinks = () => (
 
   <div className="getconnected-links">
     <p>
-      <a className="social-link" href="#"><i className="fa fa-facebook-official"></i></a>
-      <a className="social-link" href="#"><i className="fa fa-twitter-square"></i></a>
-      <a className="social-link" href="#"><i className="fa fa-pinterest-square"></i></a>
-      <a className="social-link" href="#"><i className="fa fa-github-square"></i></a>
-      <a className="social-link" href="#"><i className="fa fa-google-plus-square"></i></a>
-      <a className="social-link" href="#"><i className="fa fa-youtube-square"></i></a>
+      {
+        socialLinks.map( slink => {
+          return (
+            <a className="social-link" href={slink.link} target="_blank"><i className={slink.icon}></i></a>
+          );
+        })
+      }
     </p>
   </div>
 
