04fa99708c52d3f44427fcfe0af93259dd1d653f
Dsenza <ddescenza@gmail.com>
2016-03-14 11:38:45 -0600

    Remove serve-favicon from package.json and associated required in server files.

--- staticServer.js
+++ staticServer.js
@@ -2,7 +2,6 @@ var path = require('path');
 var express = require('express');
 var webpack = require('webpack');
 var config = require('./webpack.config.prod');
-var favicon = require('serve-favicon');
 
 var app = express();
 var compiler = webpack(config);
