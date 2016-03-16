ccf18e5fae60459f760eeb2e31313ab1138f2ea3
Devin DeScenza <ddescenza@gmail.com>
2016-03-15 22:38:15 -0600

    Fix url, wrap entire component in link, main hover transition

--- src/components/segments/MeetupDate.js
+++ src/components/segments/MeetupDate.js
@@ -21,21 +21,24 @@ class MeetupDate extends React.Component {
   render() {
 
     return (
+      
       <div className="interested-calendar">
-        <div className="flexgrid-row">
-          <div className="calendar-date">
-            <div className="month">{this.props.date.format('MMMM')}</div>
-            <div className="day">{this.props.date.format('D')}</div>
+        <a href={this.props.url} target="_blank">
+          <div className="flexgrid-row">
+            <div className="calendar-date">
+              <div className="month">{this.props.date.format('MMMM')}</div>
+              <div className="day">{this.props.date.format('D')}</div>
+            </div>
+            <div className="calendar-text">
+              <p className="meetup-btn">Join {this.props.rsvps} others at our next event!</p>
+              <p className="date">{this.props.name}</p>
+              <p className="date">
+                {this.props.date.format('dddd, MMMM Do, YYYY')}
+                <br />{this.props.date.format('h:mm A')}
+              </p>
+            </div>
           </div>
-          <div className="calendar-text">
-            <a href={this.props.event_url} target="_blank" className="meetup-btn">Join {this.props.rsvps} others at our next event!</a>
-            <p className="date">{this.props.name}</p>
-            <p className="date">
-              {this.props.date.format('dddd, MMMM Do, YYYY')}
-              <br />{this.props.date.format('h:mm A')}
-            </p>
-          </div>
-        </div>
+        </a>
       </div>
     )
   }
