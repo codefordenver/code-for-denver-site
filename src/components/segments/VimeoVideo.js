a5cdd1c6b3536c6c20613fff5c8fd6da25379873
egdavis <egdavis@live.com>
2016-03-31 18:12:54 -0600

    Added VimeoVideo segment.

---
+++ src/components/segments/VimeoVideo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Video extends React.Component {
+  render() {
+    return (
+    	<div>
+      		<iframe src="https://player.vimeo.com/video/126984194" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+      	</div>
+    )
+  }
+}
