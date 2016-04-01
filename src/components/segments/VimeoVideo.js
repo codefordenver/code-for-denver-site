d5ebf79a730d40faf82a6e0dfbcd5e5ec4484021
egdavis <egdavis@live.com>
2016-03-31 18:54:22 -0600

    Added some css for the VimeoVideo.

--- src/components/segments/VimeoVideo.js
+++ src/components/segments/VimeoVideo.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Video extends React.Component {
   render() {
     return (
-    	<div>
+    	<div className="vimeo-video">
       		<iframe src="https://player.vimeo.com/video/126984194" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
       	</div>
     )
