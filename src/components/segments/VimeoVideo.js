0bc7e392e4319f25517934045f8632431f12eb79
egdavis <egdavis@live.com>
2016-05-11 13:44:25 -0600

    Update the vimeo video html/css to display in a responsive manor.

--- src/components/segments/VimeoVideo.js
+++ src/components/segments/VimeoVideo.js
@@ -4,7 +4,7 @@ export default class Video extends React.Component {
   render() {
     return (
     	<div className="vimeo-video">
-      		<iframe src="https://player.vimeo.com/video/126984194" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+      		<iframe src="https://player.vimeo.com/video/126984194" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
       	</div>
     )
   }
