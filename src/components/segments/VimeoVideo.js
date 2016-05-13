2b2116da64e303ff3e31ab8baca0587b0989079e
egdavis <egdavis@live.com>
2016-05-12 18:40:42 -0600

    The margin on the iframe was overflowing the vimeo-video containter on some small resolution/browser combonations. A new wrapper has been added and the margin on the iframe was removed.

--- src/components/segments/VimeoVideo.js
+++ src/components/segments/VimeoVideo.js
@@ -4,7 +4,7 @@ export default class Video extends React.Component {
   render() {
     return (
     	<div className="vimeo-video">
-      		<iframe src="https://player.vimeo.com/video/126984194" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+      		<iframe src="https://player.vimeo.com/video/126984194?color=ffffff&byline=0&portrait=0&badge=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
       	</div>
     )
   }
