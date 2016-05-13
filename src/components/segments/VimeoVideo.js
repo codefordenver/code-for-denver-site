504533fb4b35680bcceede32f8cbf76a6b81a52a
egdavis <egdavis@live.com>
2016-05-12 19:01:16 -0600

    Fixed formatting in VimeoVideo segment and removed overflow css rule.

--- src/components/segments/VimeoVideo.js
+++ src/components/segments/VimeoVideo.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default class Video extends React.Component {
   render() {
     return (
-    	<div className="video-wrapper">
-			<div className="vimeo-video">
-		  		<iframe src="https://player.vimeo.com/video/126984194?color=ffffff&byline=0&portrait=0&badge=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-		  	</div>
-		</div>
+      <div className="video-wrapper">
+        <div className="vimeo-video">
+            <iframe src="https://player.vimeo.com/video/126984194?color=ffffff&byline=0&portrait=0&badge=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          </div>
+      </div>
     )
   }
 }
